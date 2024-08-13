d66d069263b18ff8c8c3404870267451e80ee15b
DevikaSajith <devikasajith710@gmail.com>
2024-08-13 12:27:25 +0530

    Add files via upload

---
+++ S3/OOPS/TUTORIAL/primecomment.java
@@ -0,0 +1,31 @@
+//program to find prime numbers numbers
+//Devika P Sajith
+//Roll no:26
+import java.util.*;
+class primecomment
+{
+	public static void main(String args[])
+	{
+		int n=0,count=0;
+		int num=Integer.parseInt(args[0]);
+		for(int i=1;i<=num;i++)
+		{
+			if(num%i==0)
+			{
+				 count=2;
+				 if(count==2)
+				 {
+			         System.out.println("the prime numbers upto input is"+n);
+			         }
+		}
+		
+		
+		}
+}
+}
+
+			
+			
+			
+			
+		
