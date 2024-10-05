a6192ae20c59594565fae28687e1baca1473753d
Devika P Sajith <devikasajith710@gmail.com>
2024-10-05 11:49:41 +0530

    Add files via upload

---
+++ S3/OOPS/LAB/myclass.java
@@ -0,0 +1,20 @@
+public class myclass
+{
+	public static void main(String args[])
+	{
+	 	try
+	 	{
+		int []num={1,2,3};
+		System.out.println(num[10]);
+		}
+		catch(Exception e)
+		{
+			System.out.println("error occured in the program");
+		}
+		finally
+		{
+			System.out.println("try and catch executed successfully");
+			}
+		
+	}
+	}
