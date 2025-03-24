c75ab225de311e58b97c2ec10dff6125dedbc5f0
Devika P Sajith <devikasajith710@gmail.com>
2025-03-24 11:54:41 +0530

    Add files via upload

--- S4/OS/LAB/bankers.c
+++ S4/OS/LAB/bankers.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    int n, m, i, j, k;
-    
+    int n, m, i, j, k, count = 0;
     printf("Enter number of processes and resources: ");
     scanf("%d %d", &n, &m);
-
-    int alloc[n][m], max[n][m], avail[m], need[n][m], work[m], finish[n], safeSeq[n];
-
-    printf("Enter allocation matrix:\n");
+    
+    int alloc[n][m], max[n][m], need[n][m], avail[m], work[m], finish[n], safeSeq[n];
+    
+    printf("Enter allocation matrix: ");
     for(i = 0; i < n; i++)
         for(j = 0; j < m; j++)
             scanf("%d", &alloc[i][j]);
-
-    printf("Enter max matrix:\n");
+    
+    printf("Enter max matrix: ");
     for(i = 0; i < n; i++)
         for(j = 0; j < m; j++)
             scanf("%d", &max[i][j]);
-
+    
     printf("Enter available resources: ");
     for(i = 0; i < m; i++)
         scanf("%d", &avail[i]);
-
+    
     for(i = 0; i < n; i++)
         for(j = 0; j < m; j++)
             need[i][j] = max[i][j] - alloc[i][j];
-
+    
     for(i = 0; i < m; i++) work[i] = avail[i];
     for(i = 0; i < n; i++) finish[i] = 0;
-
-    int count = 0;
+    
     while(count < n) {
         int found = 0;
         for(i = 0; i < n; i++) {
@@ -55,11 +53,13 @@ int main() {
             return 0;
         }
     }
-
+    
     printf("Safe sequence: ");
     for(i = 0; i < n; i++)
         printf("P%d ", safeSeq[i]);
     printf("\n");
-
+    
     return 0;
 }
+
+
